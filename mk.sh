e30772fca7207c7fdc349f46b760620a9ea16b67
Juan Esteban <juanes10ospina@hotmail.com>
2020-07-13 23:30:07 -0500

    branch

---
+++ mk.sh
@@ -0,0 +1,29 @@
+[33mcommit cccc9ed9d6fbbeb77b040d4661fee5b26c65f593[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Juan Esteban <juanes10ospina@hotmail.com>
+Date:   Mon Jul 13 23:21:32 2020 -0500
+
+    commit
+
+[33mcommit eccf4a7528e5ffe2ce2d27b24143855c2ab53876[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: CarlitosJPYT <59419806+CarlitosJPYT@users.noreply.github.com>
+Date:   Mon Jul 13 22:54:37 2020 -0500
+
+    Create README.md
+
+[33mcommit 6d6755a12973ab4f1f61efe8ffb7bf0771c5bbbc[m
+Author: Juan Esteban <juanpana10ospina@gmail.com>
+Date:   Mon Jul 13 21:54:25 2020 -0500
+
+    add
+
+[33mcommit c8bf34e384b772dfdc0d9cd488768bbd8d8c4baf[m
+Author: Carlitos <juanes10ospina@hotmail.com>
+Date:   Mon Jul 13 21:30:17 2020 -0500
+
+    modified
+
+[33mcommit 5546f2d8850906387d4b42d9434af22433b4c8be[m
+Author: Juan Esteban <juanpana10ospina@gmail.com>
+Date:   Mon Jul 13 21:17:17 2020 -0500
+
+    Mi Primer commit
